2fc10e3177204d488b526abd6c5cd75ccd7d359c
AmaraaTs <amarjargal.ts01@gmail.com>
2024-06-12 23:47:17 +0800

    homework nemew

---
+++ hw.js
@@ -0,0 +1,78 @@
+// bodlogo1
+
+let a1 = 10;
+for (let i = 1; i <= a1; i++) {
+  console.log("Bodlogo1: ", i);
+}
+
+// bodlogo2
+
+let a2 = 10;
+for (let i = 1; i <= a2; i = i + 2) {
+  console.log("Bodlogo2: ", i);
+}
+
+// bodlogo3
+
+let a3 = 10;
+for (let i = 2; i <= a3; i = i + 2) {
+  console.log("Bodlogo2: ", i);
+}
+
+// bodlogo4
+
+let sum4 = 0;
+let a4 = 0;
+for (let i = 1; i <= 100; i++) {
+  a4 = a4 + 1;
+  sum4 = sum4 + a4;
+}
+console.log("Bodlogo4: ", sum4);
+
+// bodlogo5
+
+let sum5 = 0;
+let a5 = 0;
+let n5 = 101;
+for (let i = 1; i <= n5; i++) {
+  a5 = a5 + 1;
+  sum5 = sum5 + a5;
+}
+console.log("Bodlogo5: ", sum5);
+
+// bodlogo6
+
+let fac6 = 1;
+let a6 = 0;
+let n6 = 4;
+for (let i = 1; i <= n6; i++) {
+  a6 = a6 + 1;
+  fac6 = fac6 * a6;
+}
+console.log("Bodlogo6: ", fac6);
+
+//bodlogo7
+
+let a7 = 1234;
+
+//bodlogo8
+
+//bodlogo9
+
+let n9 = 15;
+let sum9 = 0;
+for (let i = 1; i <= n9; i = i + 2) {
+  console.log("Bodlogo9: ", i);
+  sum9 = sum9 + i;
+}
+console.log("Bodlogo9: ", sum9);
+
+//bodlogo10
+
+let n10 = 15;
+let sum10 = 0;
+for (let i = 2; i <= n9; i = i + 2) {
+  console.log("Bodlogo10: ", i);
+  sum10 = sum10 + i;
+}
+console.log("Bodlogo10: ", sum10);
